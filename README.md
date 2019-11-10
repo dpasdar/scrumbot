d5d826d236bea2a357fac8ea5ec453fc7176894f
David Pasdar <dpasdar@gmail.com>
2019-11-10 09:57:41 +0100

    Added a more convenient way to participate in group estimation

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # SDK4 Teams Scrum Poker bot with Typescript
-![](recording.gif)
-This is an scrum poker chatbot built on top of the Microsoft Bot Framework Node.JS SDK v4. It will support group conversation in a channel by mentioning the name of the bot and the ticket number of a JIRA ticket as well as a simple dialog to assist users in estimation of tickets.
+![](screen-recording.gif)
+
+This is an scrum poker chatbot built on top of the Microsoft Bot Framework Node.JS SDK v4 to help with group ticket estimations. 
+The main use-case is in a group-chat/channel setup, in which each of the remote participants will 
+contribute to the ticket estimation without seeing each others estimations. It also has direct
+integration with JIRA, therefore it can display the ticket summary and submit the ticket estimation directly to JIRA.
 
 Please create and adjust the .env file accordingly. 
-The following env variables are required to write/read from JIRA :
+The following env variables are required :
+
+**MICROSOFT_APP_ID**= The azure APP_ID from azure portal
+
+**MICROSOFT_APP_PASSWORD**= registered APP_PASSWORD from azure portal
+
+**ENV**=PROD
+
+**JIRA_TICKET_PREFIX**= optional prefix the filter JIRA tickets
 
 **JIRA_SERVER_URL**= https address of the JIRA server, rest api version 2 endpoint should be accessible
 
--- src/bots/bot.ts
+++ src/bots/bot.ts
@@ -8,14 +8,16 @@ export class ScrumJiraBot extends ActivityHandler {
     private jiraServerUrl: string;
     private jiraEstimationField: string;
     private jiraAuthHeader: string;
+    private jiraTicketPrefix: string;
     constructor(private conversationState: ConversationState) {
         super();
         this.jiraServerUrl = process.env.JIRA_SERVER_URL;
         this.jiraEstimationField = process.env.JIRA_ESTIMATION_CUSTOM_FIELD_NAME;
         this.jiraAuthHeader = process.env.JIRA_AUTH_HEADER;
+        this.jiraTicketPrefix = process.env.JIRA_TICKET_PREFIX || '';
         if(!this.jiraServerUrl || !this.jiraEstimationField || !this.jiraAuthHeader)
             throw new Error('Missing Required JIRA settings');
-        if (!conversationState) 
+        if (!conversationState)
             throw new Error('conversationState is required');
 
         // Create the state property accessors for the conversation data and user profile.
@@ -29,29 +31,10 @@ export class ScrumJiraBot extends ActivityHandler {
             } else if (!conversationData.ticket) {
                 await this.processTicketNumber(turnContext, conversationData);
                 if (!conversationData.ticket) {
-                    await turnContext.sendActivity('Which ticket number you would like to estimate?');
+                    await turnContext.sendActivity('Which ticket num you would like to estimate?');
                 }
             } else {
-                const possibleEstimation = this.removeAt(turnContext.activity.text);
-                if(possibleEstimation.match(/^\d+$/)) {
-                    console.log('received estimation', possibleEstimation);
-                    conversationData.estimates.push(possibleEstimation);
-                } else {
-                    console.log('received quit', conversationData);
-                    if(conversationData.estimates.length > 0) {
-                        var card = estimationCard;
-                        card.body[4].value = Math.max(...conversationData.estimates).toString();
-                        card.body[4].choices = conversationData.estimates.map((s: string) => ({title: s, value: s}));
-                        card.body[1].text = conversationData.ticket.key;
-                        card.body[2].text = conversationData.ticket.summary;
-                        await turnContext.sendActivity({
-                            attachments: [CardFactory.adaptiveCard(card)]
-                        });
-                    } else {
-                        this.cancel(conversationData);
-                    }
-                }
-                
+                this.cancel(conversationData);
             }
             await next();
         });
@@ -65,14 +48,32 @@ export class ScrumJiraBot extends ActivityHandler {
     async processSubmitAction(turnContext, conversationData) {
         const activityValue = turnContext.activity.value;
         console.log('received activity', activityValue)
-        if(activityValue.action === 'save' && (activityValue.estimate || activityValue.customEstimate)) {
+        if(activityValue.action === 'cancel') {
+            this.cancel(conversationData);
+        } else if(activityValue.action === 'add' && activityValue.userEstimate) {
+            conversationData.estimates.push(activityValue.userEstimate);
+        } else if(activityValue.action === 'finish') {
+            if(conversationData.estimates.length > 0) {
+                var card = estimationCard;
+                card.body[4].value = Math.max(...conversationData.estimates).toString();
+                card.body[4].choices = conversationData.estimates.map((s: string) => ({title: s, value: s}));
+                card.body[1].text = conversationData.ticket.key;
+                card.body[2].text = conversationData.ticket.summary;
+                await turnContext.sendActivity({
+                    attachments: [CardFactory.adaptiveCard(card)]
+                });
+            } else {
+                this.cancel(conversationData);
+            }
+        } else if(activityValue.action === 'save' && (activityValue.estimate || activityValue.customEstimate)) {
             const estimate = activityValue.customEstimate ? activityValue.customEstimate : activityValue.estimate;
             if(estimate) {
                 await turnContext.sendActivity(`${ conversationData.ticket.key }: Publishing ${estimate} to JIRA`);
                 await this.publishToJira(conversationData.ticket, estimate);
             }
+            this.cancel(conversationData);
         }
-        this.cancel(conversationData);
+
     }
     private cancel(conversationData) {
         conversationData.ticket = null;
@@ -82,19 +83,19 @@ export class ScrumJiraBot extends ActivityHandler {
         console.log('possible ticket number to send to JIRA', ticketNumber);
         var options = {
             method: 'GET',
-              uri: `${this.jiraServerUrl}/rest/api/2/search?maxResults=1&validateQuery=false&jql=id=${ticketNumber}`,
-              json: true,
-              headers : {
-                  'Authorization': `Basic ${this.jiraAuthHeader}`
-              }
-          };
- 
-         const result = await request.get(options);
-         if(result && result.issues && result.issues.length > 0) {
-            return new JiraTicket(result.issues[0].key, 
-                result.issues[0].fields.summary, 
+            uri: `${this.jiraServerUrl}/rest/api/2/search?maxResults=1&validateQuery=false&jql=id=${ticketNumber}`,
+            json: true,
+            headers : {
+                'Authorization': `Basic ${this.jiraAuthHeader}`
+            }
+        };
+
+        const result = await request.get(options);
+        if(result && result.issues && result.issues.length > 0) {
+            return new JiraTicket(result.issues[0].key,
+                result.issues[0].fields.summary,
                 result.issues[0].self, result.issues[0].fields[this.jiraEstimationField]) ;
-         }
+        }
     }
     async publishToJira(ticket: JiraTicket, estimate) {
         var options = {
@@ -109,13 +110,13 @@ export class ScrumJiraBot extends ActivityHandler {
                     [this.jiraEstimationField] : +estimate
                 }
             }
-          };
- 
-         return request.put(options);
+        };
+
+        return request.put(options);
     }
     async processTicketNumber(turnContext, conversationData) {
         const possibleTicketNumber = turnContext.activity.text.replace(/<at>.*<\/at>/, '').trim();
-        if(possibleTicketNumber.match(/^PDCR-\d+$/)) {
+        if(possibleTicketNumber.match(new RegExp(`^${this.jiraTicketPrefix}`))) {
             const ticket = await this.getJiraTicketInfo(possibleTicketNumber);
             if(ticket) {
                 conversationData.ticket = ticket;
@@ -135,9 +136,9 @@ export class ScrumJiraBot extends ActivityHandler {
 }
 
 export class JiraTicket {
-    constructor(public key: string, 
-        public summary: string, 
-        public link:string,
-        public estimation:string
+    constructor(public key: string,
+                public summary: string,
+                public link:string,
+                public estimation:string
     ) {}
 }
